54a964c90a57a3b2b938fa3dd122fc39a321fbfd
Marcin Szczęsny <szczesny.marcin@outlook.com>
2023-04-28 13:13:01 +0200

    Chapter4 - palindron - final

--- Chapter4/Palindron.py
+++ Chapter4/Palindron.py
@@ -1,10 +1,9 @@
 def palindron(text):
     for i in range(0, len(text) // 2 + 1):
-        print(i)
         if text[i] == text[len(text) - i - 1]:
             continue
         else:
             return False
     return True
 print(palindron('kajak'))
-print(palindron('potop'))
+print(palindron('potok'))
