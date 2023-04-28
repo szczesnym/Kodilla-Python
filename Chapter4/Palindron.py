eced0bf4164f1ce8834e388551afd42688d75243
Marcin Szczęsny <szczesny.marcin@outlook.com>
2023-04-28 13:15:57 +0200

    Chapter4 - palindron - comments

--- Chapter4/Palindron.py
+++ Chapter4/Palindron.py
@@ -1,4 +1,9 @@
 def palindron(text):
+    """
+
+    :param text: Text to check if it's palindrome
+    :return: True if the text IS palindrome, False in other cases
+    """
     for i in range(0, len(text) // 2 + 1):
         if text[i] == text[len(text) - i - 1]:
             continue
